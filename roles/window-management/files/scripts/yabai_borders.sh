eec5a8f2aa4012505559293963daaa7b1b28ce72
Nauticus <emanuil.d.ganchev@gmail.com>
2023-05-06 08:18:37 +0300

    add git WIP

--- roles/window-management/files/scripts/yabai_borders.sh
+++ roles/window-management/files/scripts/yabai_borders.sh
@@ -3,7 +3,6 @@
 layout=$(yabai -m query --spaces --space | jq -re '.["type"]')
 windows_length=$(yabai -m query --spaces --space | jq -re '.["windows"] | length')
 
-
 if [[ "$layout" == "bsp" ]]; then
     if [[ $windows_length == 1 ]]; then
         yabai -m config window_border off
